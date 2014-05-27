9bc97a43420bf18396c3cc46df1cbba779ed0398
Chris Mackle <macklec@wit.edu>
2014-05-26 22:39:02 -0700

    Added bind and connect functions

--- ToothPaste/ToothPaste/ToothPaste.cpp
+++ ToothPaste/ToothPaste/ToothPaste.cpp
@@ -16,6 +16,7 @@ using namespace std;
 int main(){
 	//Initialising winsock
 	WSADATA data;
+	SOCKADDR_BTH sab, sab2;
 	int result;
 	result = WSAStartup(MAKEWORD(2, 2), &data);//initializing winsock
 	if (result!=0){
@@ -41,18 +42,76 @@ int main(){
 	memset(buffer, 0, sizeof(buffer));
 	DWORD bufferLength = sizeof(buffer);
 	WSAQUERYSET *pResults = (WSAQUERYSET*)&buffer;
-	while (result==0){
+	while (result==0)
+	{
 		result = WSALookupServiceNext(hLookup, LUP_RETURN_NAME | LUP_CONTAINERS | LUP_RETURN_ADDR | LUP_FLUSHCACHE | LUP_RETURN_TYPE | LUP_RETURN_BLOB | LUP_RES_SERVICE,&bufferLength, pResults);
 		if(result==0){// A device found
 			//print the name of the device
 			LPTSTR s = pResults->lpszServiceInstanceName;
+			BTH_ADDR btAddress = ((SOCKADDR_BTH *)pResults->lpcsaBuffer->RemoteAddr.lpSockaddr)->btAddr;
+			LPGUID serviceClass = pResults->lpServiceClassId;
 			wcout << s << " found. \n";
+			wprintf(L"Device Address is 0X%012X\n", btAddress);
+			wcout << "GUID: " << serviceClass << endl;
+			wcout << "Connect to this device? (y/n) \n";
+
+			string response;
+			cin >> response;
+			if (response == "y" || response == "yes" || response == "Y" || response == "Yes")
+			{
+				SOCKET sock = socket (AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
+				if (sock == INVALID_SOCKET)
+				{
+					printf ("Socket creation failed, error %d\n", WSAGetLastError());
+					return 1;
+				}
+				else
+					printf ("socket() looks fine!\n");
+	
+				sab.addressFamily = AF_BTH;
+				sab.btAddr = 0;
+				sab.serviceClassId = GUID_NULL;
+				sab.port = 0;
+
+				int bindResult = 0;
+				bindResult = bind (sock, (SOCKADDR *) &sab, sizeof (sab));
+				if (bindResult == SOCKET_ERROR) {
+					wprintf(L"Bind failed with error %u\n", WSAGetLastError());
+					closesocket(sock);
+					WSACleanup();
+					return 1;
+				}
+				else
+					wprintf(L"Bind returned success\n");	
+
+				int connectResult = 0;
+				sab2.addressFamily = AF_BTH;
+				sab2.btAddr = btAddress;
+				sab2.port = 0;
+				sab2.serviceClassId = *serviceClass;
+				
+				connectResult = connect(sock, (SOCKADDR *) &sab2, sizeof(sab2));
+				if (connectResult == SOCKET_ERROR) {
+					wprintf(L"connect function failed with error: %ld\n", WSAGetLastError());
+					connectResult = closesocket(sock);
+					if (connectResult == SOCKET_ERROR)
+						wprintf(L"closesocket function failed with error: %ld\n", WSAGetLastError());
+					WSACleanup();
+					return 1;
+				}
+				wprintf(L"Connected!!!!!!!!!!!!.\n");
+				Sleep(10000);
+				WSACleanup();
+				closesocket(sock);
+				shutdown(sock, 2);
+			}						
 		}
 	}
-	cout << "Enter the name of device you'd like to connect to. \n";
-	string deviceToConnect;
-	getline(cin, deviceToConnect);
-	cout << deviceToConnect + "\n";
-	SOCKET s = socket (AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
+	
+
+
+	WSACleanup();
+	//closesocket(s);
+	//shutdown(s, 2);
 	
 }
